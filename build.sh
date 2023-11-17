c99fd6b5fb9e1b88ae95a1bdab99cb84553eaf1c
JrGustavo <gus@soypetal.com>
2023-11-17 15:39:39 -0500

    Update Part#2

--- build.sh
+++ build.sh
@@ -16,7 +16,18 @@ export GOARCH=amd64
 # Compilar el código
 go build main.go
 
+# Verificar que el archivo main existe
+if [ ! -f main ]; then
+  echo "El archivo main no existe."
+  exit 1
+fi
 
 # Agregar el archivo ejecutable compilado al archivo zip
 zip -r main.zip . -i main
+
+
+# Comprobar el contenido del archivo zip
+unzip main.zip
+
+
 
